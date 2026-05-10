3a1ab3e61e208c59e2c21ba1d8ab902c0214f69c
Tsahisa <tsaqif.hisyam2005@gmail.com>
2026-05-10 19:38:21 +0700

    Initial commit: Menambahkan file proyek Java

---
+++ src/main/java/com/example/Driver.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.logging.Logger;
+
+public class Driver {
+	private static final Logger logger = Logger.getLogger(Driver.class.getName());
+	private static final String COUNT_PREFIX = "Count:";
+
+	public static void main(String[] args) {
+		Counter counter = new Counter();
+
+		counter.increment();
+		counter.increment();
+		counter.increment();
+		logger.info(COUNT_PREFIX + counter.getCount());
+
+		counter.decrement();
+		logger.info(COUNT_PREFIX + counter.getCount());
+
+		counter.reset();
+		logger.info(COUNT_PREFIX + counter.getCount());
+	}
+}
